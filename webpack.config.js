fb4d70b0053d26cce50a272d6b58046a06f5ac0a
Tenzin <sinzin91@gmail.com>
2016-05-17 16:08:42 -0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+
+const NpmInstallPlugin = require('npm-install-webpack-plugin');
+
+const TARGET = process.env.npm_lifecycle_event;
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+const common = {
+  devtool: 'eval-source-map',
+  // Entry accepts a path or an object of entries.  We'll be using the 
+  // latter form given its convenient with more complex configs.
+  entry: {
+    app: PATHS.app
+  },
+  // Add resolve.extensions.
+  // '' is needed to allow imports without an extension.
+  // Note the .'s before extensions as it will fail to match without it!
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        // Test expects a RegExp!
+        test: /\.css$/,
+        loaders: ['style', 'css'], // evaluated from right to left
+        // Include accepts either a path or an array of paths.
+        // If include is not set, Webpack will traverse all files in directory
+        include: PATHS.app
+      },
+      // Set up jsx.  This accepts js too thanks to RegExp
+      {
+        test: /\.jsx?$/,
+        // Enable caching for improved performance during development
+        // It uses default OS directory by default.  If you need something
+        // more custom, pass a path to it.  I.e., babel?cacheDirectory=<path>
+        loaders: ['babel?cacheDirectory'],
+        // Parse only app files!  Without this it will go through entire project.
+        // In addition to being slow, that will most likely result in an error.
+        include: PATHS.app
+      }
+    ]
+  }
+};
+
+// Default config.  We will return this if 
+// Webpack is called outside of npm.
+if (TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devServer: {
+      contentBase: PATHS.build,
+
+      // Enable history API fallback so HTML5 History API based
+      // routing works.  This is a good default that will come
+      // in handy in more complicated setups.
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+
+      // Display only errors to reduce the amount of output.
+      stats: 'errors-only',
+
+      // Parse host and port from env so this is easy to customize.
+      //
+      // If you use Vagrant or Cloud9, set
+      // host: process.env.HOST || '0.0.0.0';
+      //
+      // 0.0.0.0 is available to all network devices unlike default
+      // localhost
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new NpmInstallPlugin({
+        save: true // --save
+      })
+    ]
+  });
+}
+
+if (TARGET === 'build') {
+  module.exports = merge(common, {});
+}
